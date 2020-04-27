8eb8f688c87e4a87c6b037828f2eb70fc3bb440c
zhaozhenwei <zhaozhenwei@sinosoft.com.cn>
2020-04-27 21:09:46 +0800

    add a test

---
+++ src/com/zzw/test/Test.java
@@ -0,0 +1,13 @@
+package com.zzw.test;
+
+/**
+ * @author zhaozhenwei
+ * @date 2020/4/27 - 21:02
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
