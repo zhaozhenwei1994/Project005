5e5da76161d7b0182fa2a4893aee3ad1881ffa95
zhaozhenwei <zhaozhenwei@sinosoft.com.cn>
2020-04-27 21:12:09 +0800

    modify test

--- src/com/zzw/test/Test.java
+++ src/com/zzw/test/Test.java
@@ -8,6 +8,11 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(100);
+        }
+
     }
 
 }
